acda9e188a0a4d90045dda1e93acd72f644ed237
Kris <87684316+KrisTribbeck@users.noreply.github.com>
2022-10-24 17:14:48 +0800

    Add files via upload

---
+++ GeneralForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace MasterFileProject
+{
+    partial class GeneralForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readOnlyListbox = new System.Windows.Forms.ListBox();
+            this.staffIdTextbox = new System.Windows.Forms.TextBox();
+            this.staffNameTextbox = new System.Windows.Forms.TextBox();
+            this.filteredListbox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.helplistTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // readOnlyListbox
+            // 
+            this.readOnlyListbox.FormattingEnabled = true;
+            this.readOnlyListbox.Location = new System.Drawing.Point(12, 37);
+            this.readOnlyListbox.Name = "readOnlyListbox";
+            this.readOnlyListbox.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.readOnlyListbox.Size = new System.Drawing.Size(209, 342);
+            this.readOnlyListbox.TabIndex = 3;
+            // 
+            // staffIdTextbox
+            // 
+            this.staffIdTextbox.Location = new System.Drawing.Point(239, 37);
+            this.staffIdTextbox.Name = "staffIdTextbox";
+            this.staffIdTextbox.Size = new System.Drawing.Size(209, 20);
+            this.staffIdTextbox.TabIndex = 0;
+            this.staffIdTextbox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.staffIdTextbox_KeyUp);
+            // 
+            // staffNameTextbox
+            // 
+            this.staffNameTextbox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.staffNameTextbox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.staffNameTextbox.Location = new System.Drawing.Point(239, 76);
+            this.staffNameTextbox.Name = "staffNameTextbox";
+            this.staffNameTextbox.Size = new System.Drawing.Size(209, 20);
+            this.staffNameTextbox.TabIndex = 1;
+            this.staffNameTextbox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.staffNameTextbox_KeyUp);
+            // 
+            // filteredListbox
+            // 
+            this.filteredListbox.FormattingEnabled = true;
+            this.filteredListbox.Location = new System.Drawing.Point(239, 102);
+            this.filteredListbox.Name = "filteredListbox";
+            this.filteredListbox.Size = new System.Drawing.Size(209, 277);
+            this.filteredListbox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(236, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Staff ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Staff Name";
+            // 
+            // helplistTextbox
+            // 
+            this.helplistTextbox.Location = new System.Drawing.Point(466, 37);
+            this.helplistTextbox.Multiline = true;
+            this.helplistTextbox.Name = "helplistTextbox";
+            this.helplistTextbox.ReadOnly = true;
+            this.helplistTextbox.Size = new System.Drawing.Size(124, 149);
+            this.helplistTextbox.TabIndex = 6;
+            this.helplistTextbox.Text = "Helplist ";
+            // 
+            // GeneralForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 405);
+            this.Controls.Add(this.helplistTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filteredListbox);
+            this.Controls.Add(this.staffNameTextbox);
+            this.Controls.Add(this.staffIdTextbox);
+            this.Controls.Add(this.readOnlyListbox);
+            this.Name = "GeneralForm";
+            this.Text = "Master File - General Form";
+            this.Load += new System.EventHandler(this.GeneralForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox readOnlyListbox;
+        private System.Windows.Forms.TextBox staffIdTextbox;
+        private System.Windows.Forms.TextBox staffNameTextbox;
+        private System.Windows.Forms.ListBox filteredListbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox helplistTextbox;
+    }
+}
+
---
+++ GeneralForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace MasterFileProject
+{
+    public partial class GeneralForm : Form
+    {
+        public GeneralForm()
+        {
+            InitializeComponent();
+        }
+        // 4.1.	Create a Dictionary data structure with a TKey of type integer
+        // and a TValue of type string, name the new structure “MasterFile”.
+        static Dictionary<int, string> MasterFile = new Dictionary<int, string>();
+        // 4.2.	Create a method that will read the data from the .csv file
+        // into the Dictionary data structure when the form loads.
+        #region Form Load
+        private void GeneralForm_Load(object sender, EventArgs e)
+        {
+            ReadStaffDetailsFromFile();
+        }
+        #endregion
+        #region Read from cvs file
+        private void ReadStaffDetailsFromFile()
+        {
+            var filePath = @"MalinStaffNamesV2.csv";
+
+            if (File.Exists(filePath))
+            {
+                string[] staffData = File.ReadAllLines(filePath);
+                foreach (var staff in staffData)
+                {
+                    var splitData = staff.Split(',');
+                    if (splitData.Length > 0)
+                    {
+                        var staffID = splitData[0];
+                        var staffName = splitData[1];
+                        MasterFile.Add(int.Parse(staffID), staffName);
+                    }
+                }
+                DisplayData(readOnlyListbox);
+            }
+            else
+            {
+                MessageBox.Show("File did not load.");
+            }
+        }
+        #endregion
+        // 4.3.	Create a method to display the Dictionary data into a non-selectable display only listbox (ie read only).
+        #region Display Data
+        private void DisplayData(ListBox listbox)
+        {
+            //readOnlyListbox.Items.Clear();
+            listbox.Items.Clear();
+            foreach (var staff in MasterFile)
+            {
+                listbox.Items.Add(staff.Key + "\t" + staff.Value);
+                //readOnlyListbox.Items.Add(staff.Key + "\t" + staff.Value);
+            }
+        }
+        #endregion
+        // 4.4.	Create a method to filter the Staff Name data from the Dictionary into a second filtered and selectable listbox.
+        // This method must use a textbox input and update as each character is entered. The listbox must reflect the filtered data in real time.
+        private void FilterStaffName()
+        {
+
+            foreach (var staff in MasterFile)
+            {
+                if (staff.Value.Contains(staffNameTextbox.Text))
+                {
+                    filteredListbox.Items.Add(staff.Key + "\t" + staff.Value);
+                }
+            }
+        }
+
+        private void FilterStaffId()
+        {
+            foreach (var staff in MasterFile)
+            {
+                if (staff.Key == int.Parse(staffIdTextbox.Text))
+                {
+                    filteredListbox.Items.Add(staff.Key + "\t" + staff.Value);
+                }
+            }
+        }
+        private void staffNameTextbox_KeyUp(object sender, KeyEventArgs e)
+        {
+            filteredListbox.Items.Clear();
+            FilterStaffName();
+        }
+
+        private void staffIdTextbox_KeyUp(object sender, KeyEventArgs e)
+        {
+            filteredListbox.Items.Clear();
+            FilterStaffId();
+        }
+    }
+}
+
